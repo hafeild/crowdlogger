791d17780378c245079f03ec59b60eb3781dcee2
Henry Feild <hafeild@gmail.com>
2013-11-26 21:47:45 -0500

    Lots more added to the App Builder.
    - syntax highlighting (using codemirror)
    - side-by-side file listing + coding window
    - frequent backups / automatic saving
    - data re-populates on refresh (to whatever was last loaded)
    
    We're getting there...

--- unconfigured/web/html/app-builder/app-builder.js
+++ unconfigured/web/html/app-builder/app-builder.js
@@ -9,20 +9,34 @@
  * @version %%VERSION%%
  */
 var AppBuilder = function() {
+    // Private constants.
+    const SAVE_FREQ = 2000;
+
     // Private vars.
     var that = this,
         appPackage,
-        currentHTML,
-        populating = false;
+        currentFileName,
+        currentFileType,
+        jqJS = jQuery('.js-editor'),
+        jqCSS = jQuery('.css-editor'),
+        htmlJS = jQuery('.html-editor'),
+        jsEditor,
+        cssEditor,
+        populating = false,
+        saving = false,
+        isDirty = false;
 
     // Private functions.
     var initTinyMCE,
         initListeners,
-        saveCurrentHTML,
+        initSyntaxHiglighting,
+        saveCurrent,
+        saveMetadata,
         createNewAppPackage,
         createAppPackageFromString,
         populateData,
-        backup;
+        backup,
+        openFileInEditor;
 
     // Public vars.
 
@@ -38,18 +52,53 @@ var AppBuilder = function() {
      */
     initTinyMCE = function(){
         tinymce.init({
+            height: '240px',
             selector:'.html-editor',
             //theme : "modern",
-            toolbar: 'save | undo redo | styleselect | fontsizeselect | '+
-                     'fontselect | table | bold italic | code',
-            plugins: 'code,table,save,print,paste,fullscreen,visualchars',
+            toolbar: 'undo redo | bold italic underline | fontsizeselect | '+
+                     'forecolor backcolor | link unlink | code',
+            plugins: 'code,table,save,print,paste,fullscreen,visualchars,'+
+                     'textcolor,codemirror,link',
+            codemirror: {
+                indentOnInit: true, 
+                path: '../../../../../codemirror-3.20',
+                config: {
+                   mode: 'xml',
+                   lineNumbers: true
+                },
+                jsFiles: [
+                   'mode/xml/xml.js',
+                ]
+            },
             save_enablewhendirty: true,
-            save_onsavecallback: function(){
-                saveCurrentHTML(
-                    currentHTMLName, 
-                    tinyMCE.activeEditor.getContent() 
-                );
+            setup: function(ed){
+                ed.on('change', function(e){
+                    isDirty = true;
+                });
             }
+            // save_onsavecallback: function(){
+            //     saveCurrentHTML(
+            //         currentHTMLName, 
+            //         tinyMCE.activeEditor.getContent() 
+            //     );
+            // }
+        });
+    };
+
+    /**
+     * Initializes the package that handles syntax highlighting.
+     */
+    initSyntaxHiglighting = function() {
+        // JavaScript.
+        jsEditor = CodeMirror.fromTextArea(jqJS[0], {
+            mode: "javascript",
+            lineNumbers: true
+        });
+
+        // CSS.
+        cssEditor = CodeMirror.fromTextArea(jqCSS[0], {
+            mode: "css",
+            lineNumbers: true
         });
     };
 
@@ -57,31 +106,223 @@ var AppBuilder = function() {
      * Places listeners on buttons, etc.
      */
     initListeners = function(){
-        jQuery(document).on('click', function(e){
-            var jelm = jQuery(e.target);
-            switch(jelm.attr('id')) {
-                case 'export':
-                    exportPackage();
-            }
+
+        // Export.
+        jQuery('#export').click(exportPackage);
+
+        // Editing a file.
+        jQuery(document).delegate('.name', 'click', function(e){
+            var jqElm = jQuery(e.target);
+            console.log('Clicked!');
+            openFileInEditor(jqElm)
         });
 
-        jQuery('.app-input form').change(function(){
-            if(!populating){
-                appPackage.metadata = readInAppMetadata();
-                backup();
+        // Removing a file.
+        jQuery(document).delegate('.remove', 'click', function(e){
+            var jqElm = jQuery(e.target);
+            var fileType = jqElm.parents('.category').attr('data-type');
+            var filename = jqElm.parents('.file').find('.name').html();
+            removeFile(filename, fileType, jqElm.parents('.file'));
+        });
+
+        // Adding a file.
+        jQuery(document).delegate('.add', 'click', function(e){
+            var jqElm = jQuery(e.target);
+            var fileType = jqElm.parents('.category').attr('data-type');
+            // Read in the filename.            
+            var filename = window.prompt(
+                "Please enter a name for the new file.")
+
+            if(!filename){ return; }
+
+            // Add it to the package.
+            if( !appPackage[fileType][filename] ){
+                appPackage[fileType][filename] = '';
             }
+
+            // Display it.
+            var entry = jQuery('#file-template').clone();
+            entry.find('.name').html(filename);
+            entry.removeClass('hidden').show().attr('id', '');
+            jqElm.parents('.category').append(entry);
+        });
+
+        // To get the 'close' button to appear next to file names when hovered.
+        jQuery('body').on('mouseenter', '.file', function(){
+            jQuery(this).find('button').show();
+        }).on('mouseleave', '.file', function(){
+            jQuery(this).find('button').hide();
+        });
+
+        // Save the metadata when it changes.
+        jQuery('.app-input form').change(saveMetadata);
+
+        // Save the current file being edited every few seconds.
+        setInterval(function(){
+            if(isDirty){ saveCurrent(); } 
+        }, SAVE_FREQ);
+
+        // Listen for changes to the editors. The changes to the HTML editor 
+        // are taken care of elsewhere.
+        //jQuery('.js-editor,.css-editor').on('keyup',function(){isDirty=true;});
+        jsEditor.on('change', function(){ 
+            console.log('Marking as dirty...');
+            isDirty = true; 
+        });
+        cssEditor.on('change', function(){ 
+            console.log('Marking as dirty...');
+            isDirty = true; 
         });
     };
 
     /**
-     * Saves the given HTML to its place in the resources part of the app
-     * package.
-     *
-     * @param {string} name    The name of the HTML file to save.
-     * @param {string} content The content of the HTML file to save.
+     * Opens the current file in the editor.
      */
-    saveHTML = function(name, content) {
-        appPackage.html[name] = content;
+    openFileInEditor = function(jqElm, count){
+        if(count === undefined){ count = 10 };
+        // Try 10 times to open.
+        if(saving){ 
+            console.log('saving = true...'+ count +' tries left...');
+            if( count > 0 ){
+                setTimeout(function(){openFileInEditor(jqElm, count--);}, 100);
+            }
+            return;
+        }
+        console.log( 'Made it!');
+        saving = true;
+
+        try {
+            //var jqElm = jQuery(elm.target);
+            currentFileType = jqElm.parents('.category').attr('data-type');
+            currentFileName = jqElm.html();
+
+            console.log('filetype: '+ currentFileType);
+            console.log('filename: '+ currentFileName);
+
+            jQuery('.editor-container').hide();
+
+            switch(currentFileType) {
+                case "module":
+                    console.log('Hey!');
+                    jqJS.parent().show();
+                    //jqJS.val(appPackage.module);
+                    jsEditor.getDoc().setValue(appPackage.module);
+                    break;
+                case "js":
+                    jqJS.parent().show();
+                    //jqJS.val(appPackage.js[currentFileName] || '');
+                    jsEditor.getDoc().setValue(
+                        appPackage.js[currentFileName] || '');
+                    break;
+                case "css":
+                    jqCSS.parent().show();
+                    //cssJS.val(appPackage.css[currentFileName] || '');
+                    cssEditor.getDoc().setValue(
+                        appPackage.css[currentFileName] || '');
+                    break;
+                case "html":
+                    htmlJS.parent().show();
+                    tinyMCE.activeEditor.setContent(
+                        appPackage.html[currentFileName] || '');
+                    break;
+            }
+        } catch(e) {
+            console.log('Error: '+ e);
+        }
+
+        saving = false;
+    };
+
+    /**
+     * Removes a given file from the app and then backs up the package in local
+     * storage. If the file is currently displayed, it is no longer displayed.
+     * The file entry is removed from the list of files.
+     *
+     * @param {string} filename    The name of the file to remove.
+     * @param {string} fileType    The type of the file.
+     * @param {jQuery} jqElm       The jQuery element corresponding the file's
+     *                             entry.
+     * @parma {int}    count       The number of remaining attempts to delete
+     *                             the entry.
+     */
+    removeFile = function(filename, fileType, jqElm, count){
+        if(count === undefined){ count = 10 };
+        // Try 10 times to open.
+        if(saving){ 
+            console.log('saving = true...'+ count +' tries left...');
+            if( count > 0 ){
+                setTimeout(function(){
+                    removeFile(filename, fileType, jqElm, count--);
+                }, 100);
+            }
+            return;
+        }
+
+        saving = true;
+
+        try{
+            if( fileType === currentFileType && filename === currentFileName){
+                jQuery('.editor-container').hide();
+                currentFileName = undefined;
+                currentFileType = undefined;
+            }
+            console.log('delete appPackage["'+ fileType +'"]["'+ filename +'"]');
+            delete appPackage[fileType][filename];
+            jqElm.remove();
+
+            backup();
+        } catch(e) {
+            console.log('Error while removing file: '+ e);
+        }
+
+        saving = false;
+    }
+
+    /**
+     * Saves the metadata and performs a backup, provided the page is not
+     * being automatically populated.
+     */
+    saveMetadata = function(){
+        if(populating || saving) return;
+        saving = true;
+        try{
+            appPackage.metadata = readInAppMetadata();
+            backup();
+        } catch(e) {}
+        saving = false;
+    };
+
+    /**
+     * Saves the file currently being edited and performs a backup, provided the
+     * page is not being automatically populated.
+     */
+    saveCurrent = function(){
+        if(populating || saving) return;
+        saving = true;
+        try{
+            if( currentFileType === "module" ){
+                appPackage.module = jqJS.val();
+            } else {
+                var content;
+                switch(currentFileType){
+                    case "html":
+                        content = tinyMCE.activeEditor.getContent();
+                        break;
+                    case "css":
+                        //content = jqCSS.val();
+                        content = cssEditor.getDoc().getValue();
+                        break;
+                    case "js":
+                        // content = jqJS.val();
+                        content = jsEditor.getDoc().getValue();
+                        break;
+                }
+                appPackage[currentFileType][currentFileName] = content;
+            }
+            backup();
+        } catch(e) {}
+        isDirty = false;
+        saving = false;
     };
 
     /**
@@ -99,9 +340,9 @@ var AppBuilder = function() {
      *
      * @return An empty app package.
      */
-    createNewAppPackage = function() {
+    generateEmptyPackage = function() {
         return {
-            module: {},
+            module: '',
             html: {},
             css: {},
             js: {},
@@ -165,6 +406,10 @@ var AppBuilder = function() {
      * Exports the app as a JSON download.
      */
     exportPackage = function(){
+        // First make sure everything is saved.
+        saveMetadata();
+        saveCurrent();
+
         // Opens a new window with the app package in JSON.
         // window.open('data:application/json,'+ 
         //     encodeURI(JSON.stringify(appPackage)));
@@ -182,20 +427,39 @@ var AppBuilder = function() {
     populateData = function(){
         populating = true;
 
-        // Populate the metadata form.
-        var key;
-        for(key in appPackage.metadata){
-            if( typeof(appPackage.metadata[key]) === "object" ){
-                jQuery('input[name="'+key+'"][value="'+ 
-                    appPackage.metadata[key]+'"]').prop('checked', true);
-            } else {
-                jQuery('input[name="'+key+'"]').val(appPackage.metadata[key]);
+        try{
+            // Populate the metadata form.
+            var key;
+            for(key in appPackage.metadata){
+                if( typeof(appPackage.metadata[key]) === "object" ){
+                    jQuery('input[name="'+key+'"][value="'+ 
+                        appPackage.metadata[key]+'"]').prop('checked', true);
+                } else {
+                    jQuery('input[name="'+key+'"]').val(appPackage.metadata[key]);
+                }
+
             }
 
-        }
+            // Populate the file lists.
+            jQuery('.category').each(function(i,elm){
+                var filename,
+                    jqElm = jQuery(elm),
+                    dataType = jqElm.attr('data-type');
 
-        // Populate the file lists.
-        // TODO
+                if( dataType !== "module" ){
+                    for(filename in appPackage[dataType]){
+                        if(filename){
+                            var entry = jQuery('#file-template').clone();
+                            entry.find('.name').html(filename);
+                            entry.removeClass('hidden').show().attr('id', '');
+                            jqElm.append(entry);
+                        }
+                    }
+                }
+            });
+        } catch(e) {
+            console.log('Error while populating: '+ e);
+        }
 
         populating = false;
     };
@@ -206,7 +470,9 @@ var AppBuilder = function() {
      * Gets everything started -- initializes the editors and places listeners.
      */
     this.start = function(){
+        jQuery('#file-template').hide();
         initTinyMCE();
+        initSyntaxHiglighting();
         initListeners();
         if( Storage && localStorage.appPackage ){
             try{
